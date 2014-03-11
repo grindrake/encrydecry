9e88d994477fd8a4012a344eeee49e01ddc426d3
grindrake <grindrake@yandex.ru>
2014-03-12 00:25:18 +0400

    2

--- module/module/maincode.cpp
+++ module/module/maincode.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <fstream>
 
+using namespace std;
+
 void main()
 {
-	char input[100];
+	long n;
+	char* insides;
+	char* output;
 	FILE* f;
-	f = fopen ("text1.txt","r");
-	input = fread (&n, sizeof(char), 1, f);
+
+	f = fopen ("text1.txt","rt");
+	fseek (f, 0, SEEK_END);
+	n = ftell (f);
+	insides = new char[n];
+	output = new char[n];
+	rewind(f);
+	fread (&insides, sizeof(char), 1, f);
+
+	for (int i=0; i<30; i++)
+	{
+		fread(&(output[i]), sizeof(char), 1, f);
+		cout<<output[i];
+	}	
+
+	system("pause");
 }
