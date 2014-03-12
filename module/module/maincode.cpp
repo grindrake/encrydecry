bb6d062c7c0c9c9c6f8ad6cd18dd52ce9a392a06
grindrake <grindrake@yandex.ru>
2014-03-12 21:18:07 +0400

    12/03 evening

--- module/module/maincode.cpp
+++ module/module/maincode.cpp
@@ -6,24 +6,44 @@ using namespace std;
 
 void main()
 {
-	long n;
+	int n;
 	char* insides;
 	char* output;
 	FILE* f;
 
-	f = fopen ("text1.txt","rt");
+	f = fopen ("../../text1.txt","r");
+	if (!f) {}
+	else
+	{
 	fseek (f, 0, SEEK_END);
+	}
+	if (!f) {}
+	else
+	{
 	n = ftell (f);
+	}
+	if (!f) {}
+	else
+	{
+	rewind (f);
+	}
+
 	insides = new char[n];
 	output = new char[n];
-	rewind(f);
-	fread (&insides, sizeof(char), 1, f);
 
-	for (int i=0; i<30; i++)
+	fread (&insides, sizeof(char), 1, f);
+	
+	for (int i=0; i<(sizeof(char)*n); i++)
 	{
 		fread(&(output[i]), sizeof(char), 1, f);
 		cout<<output[i];
-	}	
+	}
+	
+	if (!f) {}
+	else
+	{
+	fclose (f);
+	}
 
 	system("pause");
 }
